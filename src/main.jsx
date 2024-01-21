4aeb2e3a54ba6c8f9688f96d624508a98a570a9e
Nur Alam <hanako.yugiamane101@gmail.com>
2024-01-22 00:01:47 +0600

    Added react router -- created home layout -- installed tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+// styling css
 import './index.css'
+
+// importing react router
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+// Components
+import Home from './components/Layout/Home.jsx'
+
+
+// route
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
