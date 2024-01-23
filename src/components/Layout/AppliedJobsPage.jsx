cabaaa84f8a64ed1c608021c4652b1cc64855673
Nur Alam <hanako.yugiamane101@gmail.com>
2024-01-23 23:51:01 +0600

    Modified jobs data

--- src/components/Layout/AppliedJobsPage.jsx
+++ src/components/Layout/AppliedJobsPage.jsx
@@ -3,6 +3,9 @@ import Footer from '../Footer/Footer';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJobs from '../AppliedJobs/AppliedJobs';
 
+// Heroicons
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
+
 
 const AppliedJobsPage = () => {
     const appliedJobsData = useLoaderData();
@@ -12,8 +15,14 @@ const AppliedJobsPage = () => {
             <header>
                 <Header>Applied Jobs</Header>
             </header>
-            <section>
-                <div className="mt-32 mx-36">
+            <section className='mt-32 mx-36'>
+                <div className='flex justify-end'>
+                    <div className='flex items-center justify-center border rounded px-3 py-2 font-medium text-dark-2 cursor-pointer'>
+                        <button className='me-2'>Filter</button>
+                        <ChevronDownIcon className='w-5 h-5' />
+                    </div>
+                </div>
+                <div>
                     {
                         appliedJobsData.map(job => <AppliedJobs key={job.id} job={job} />)
                     }
